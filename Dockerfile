7c4e0f9b4ce11cdd4fa7116ecd36b21146a34079
lalaro <100946295+lalaro@users.noreply.github.com>
2025-03-12 14:57:09 -0500

    Cambios para la conexión del back y dockerizado

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ WORKDIR /usrapp/bin
 
 ENV PORT=35000
 
+COPY /target/dependency /usrapp/bin/dependency
 COPY /target/classes /usrapp/bin/classes
 
-CMD ["java","-cp","./classes:./dependency/*","com.example.demo.DemoApplication"]
+CMD ["java","-cp","./classes:./dependency/*","edu.escuelaing.app.Application"]
--- src/main/java/edu/escuelaing/app/security/SecurityConfig.java
+++ src/main/java/edu/escuelaing/app/security/SecurityConfig.java
@@ -2,6 +2,10 @@ package edu.escuelaing.app.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -12,5 +16,21 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-}
 
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails admin = User.builder()
+                .username("laurita")
+                .password(passwordEncoder().encode("astro123"))
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user = User.builder()
+                .username("user")
+                .password(passwordEncoder().encode("user123"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+}
